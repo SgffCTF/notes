064d2fba85f8b5872bf37969bc6a143bb26942dc
Jacute <Eri228@yandex.ru>
2025-08-10 10:38:58 +0700

    update checker

--- checker/checker.py
+++ checker/checker.py
@@ -1,10 +1,8 @@
 import sys
-import requests
-import random
 import notes_api
 from checklib import *
 from checklib import status
-import re
+
 
 class Checker(BaseChecker):
 
@@ -13,50 +11,74 @@ class Checker(BaseChecker):
         self.api = notes_api.NotesApi(self, self.host)
     
     def check(self):
-        self.api.healthcheck()
+        status_code = self.api.healthcheck()
+        self.assert_eq(status_code, 200, "Healthcheck failed")
 
+        username, password = rnd_username(), rnd_password()
 
-        f_username, f_password = rnd_username(), rnd_password()
-        s_username, s_password = rnd_username(), rnd_password()
-
-        self.api.register(f_username, f_password)
-        self.api.login(f_username, f_password)
-        self.api.register(s_username, s_password)
-        self.api.login(s_username, s_password)
-        self.api.get_users()
+        self.api.register(username, password)
+        self.api.login(username, password)
+        
+        title, content = rnd_string(10), rnd_string(10)
+        note_id = self.api.add(title, content, is_public=1)
+        html_data = self.api.public()
+        
+        if title not in html_data:
+            self.cquit(Status.CORRUPT, "Заголовок заметки не совпал")
+        if content not in html_data:
+            self.cquit(Status.CORRUPT, "Содержимое заметки не совпало")
+            
+        new_content = rnd_string(10)
+        self.api.check_edit_note(note_id, new_content, is_public=1, remove_file='')
+        
+        html_data = self.api.public()
+        if new_content not in html_data:
+            self.cquit(Status.CORRUPT, "Новое содержимое заметки не совпало")
+        
+        self.api.check_delete(note_id)
+        
+        html_data = self.api.public()
+        if new_content in html_data:
+            self.cquit(Status.CORRUPT, "Заметка не была удалена")
         
         self.cquit(Status.OK)
     
     def put(self, flag_id: str, flag: str, vuln: str):
+        username, password = rnd_username(), rnd_password()
+        user_id = self.api.register(username, password)
+        if user_id == -1:
+            self.cquit(Status.CORRUPT, "Пользователь не был создан")
+        self.api.login(username, password)
         
-        f_username, f_password = rnd_username(), rnd_password()
-        self.api.register(f_username, f_password)
-        self.api.login(f_username, f_password)
+        content = rnd_string(10)
+        note_id = self.api.add(flag, content, is_public=0)
+        if note_id == -1:
+            self.cquit(Status.CORRUPT, "Заметка не была создана")
         
-        title = rnd_string(10)
-        note_id = self.api.add(title, flag, is_public=0)
-        
-        s_username, s_password = rnd_username(), rnd_password()
-        self.api.register(s_username, s_password)
-        self.api.login(s_username, s_password)
-        self.api.get_users()
-        
-        flag_id = f"{f_username}:{note_id}"
-        self.cquit(Status.OK, flag_id, flag_id)
+        public_flag_id = f"{user_id}:{note_id}"
+        private_flag_id = f"{username}:{password}:{note_id}"
+        self.cquit(Status.OK, public_flag_id, private_flag_id)
     
     def get(self, flag_id: str, flag: str, vuln: str):
         parts = flag_id.split(':')
         if len(parts) < 2:
             self.cquit(Status.CORRUPT, "Invalid flag_id", "Bad flag_id format")
         username = parts[0]
-        note_id = parts[1]
+        password = parts[1]
+        note_id = parts[2]
+
+        self.api.login(username, password)
         
-        s_username, s_password = rnd_username(), rnd_password()
-        self.api.register(s_username, s_password)
-        self.api.login(s_username, s_password)
+        html_data = self.api.my_notes()
         
-        content = self.api.get_note_content(note_id)
-        
-        if flag not in content:
+        if flag not in html_data:
             self.cquit(Status.CORRUPT, "Flag not found", f"Flag not found in note {note_id}")
         self.cquit(Status.OK)
+        
+
+if __name__ == '__main__':
+    c = Checker(sys.argv[2])
+    try:
+        c.action(sys.argv[1], *sys.argv[3:])
+    except c.get_check_finished_exception() as e:
+        cquit(status.Status(c.status), c.public, c.private)
--- checker/notes_api.py
+++ checker/notes_api.py
@@ -1,8 +1,7 @@
 import requests
+import re
 from checklib import BaseChecker
-import hashlib
 
-from flask import session
 
 PORT = 5000
 
@@ -12,55 +11,50 @@ class NotesApi:
         self.c = checker
         self.session = requests.Session()
         self.url = f'http://{host}:{port}'
-
-    def home(self):
-        r = self.session.get(url=f'{self.url}/')
+    
+    def healthcheck(self) -> int:
+        r = self.session.get(f'{self.url}/public')
+        return r.status_code
 
     def public(self):
-        r = self.session.get(url=f'{self.url}/public')
+        return self.session.get(url=f'{self.url}/public').text
+    
+    def my_notes(self):
+        return self.session.get(url=self.url).text
 
-    def register(self, username: str, password: str):
+    def register(self, username: str, password: str) -> int:
         r = self.session.post(url=f'{self.url}/register', data={'username': username, 'password': password})
 
         self.c.assert_eq(r.status_code, 200, "Статус код не совпал")
-        self.c.assert_in("", r.text, "не обнаружено ''")
+        match = re.search(r"id=(\d+)", r.text)
+        if match:
+            user_id = int(match.group(1))
+            return user_id
+        return -1
 
     def login(self, username: str, password:str):
         r = self.session.post(url=f'{self.url}/login', data={'username': username, 'password': password})
-
         self.c.assert_eq(r.status_code, 200, "Статус код не совпал")
-        self.c.assert_in("Вы успешно вошли в систему!", r.text, "не обнаружено 'Вы успешно вошли в систему!'")
 
-    def logout(self):
-        r = self.session.get(url=f'{self.url}/logout')
-
-        self.c.assert_eq(r.status_code,200, "статус код не совпал")
-        self.c.assert_in("Вы вышли из системы.", r.text, "Не обнаружено 'Вы вышли из системы.'")
-
-    def add(self, title: str, content: str, is_public: int):
+    def add(self, title: str, content: str, is_public: int) -> int:
         r = self.session.post(url=f'{self.url}/add', data={'title': title, 'content': content, 'is_public': is_public})
-
         self.c.assert_eq(r.status_code, 200, "статус код не совпал")
-        self.c.assert_in("Заметка успешно создана!", r.text, "Не обнаружено 'Заметка успешно создана!'")
-
-    def download_public_file(self, filename: str):
-        r = self.session.get(f"{self.url}/download/{filename}")
-
-        self.assert_not_in("Файл не найден или у вас нет доступа", r.text, "Файл не найден или у вас нет доступа")
-
-    def download_private_file_authorized(self, filename: str):
-        r = self.session.get(f"{self.url}/download/{filename}")
-
-        self.assert_not_in("Файл не найден или у вас нет доступа", r.text, "Файл не найден или у вас нет доступа")
-
-    def check_edit_note(self, note_id: int, title: str, content: str, is_public: int, remove_file: str):
-        r = self.session.post(url=f'{self.url}/{note_id}', data={'title': title, 'content': content, 'is_public': is_public, 'remove_file': remove_file})
         
-        self.c.assert_eq(r.status_code, 200, "статус код не совпал")
-        self.c.assert_in("Заметка обновлена!", r.text, "Не обнаружено 'Заметка обновлена!")
+        match = re.search(r"id=(\d+)", r.text)
+        if match:
+            note_id = int(match.group(1))
+            return note_id
+        return -1
 
-    def check_delete(self, note_id: int, title: str, content: str):
-        r = self.session.get(f'{self.url}/delete/{note_id}', allow_redirects=True)
+    def download_file(self, filename: str):
+        r = self.session.get(f"{self.url}/download/{filename}")
 
-        self.assert_not_in("Ошибка при удалении файла", r.text, "Ошибка при удалении файла")
+        self.assert_not_in("Файл не найден или у вас нет доступа", r.text, "Файл не найден или у вас нет доступа")
+
+    def check_edit_note(self, note_id: int, content: str, is_public: int, remove_file: str):
+        r = self.session.post(url=f'{self.url}/edit/{note_id}', data={'content': content, 'is_public': is_public, 'remove_file': remove_file})        
+        self.c.assert_eq(r.status_code, 200, "Обновление заметки не работает")
+
+    def check_delete(self, note_id: int):
+        self.session.get(f'{self.url}/delete/{note_id}', allow_redirects=True)
 
